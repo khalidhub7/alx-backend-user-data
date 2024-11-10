e1f807a46a821196c2840dddcc9949b040eef7f5
khalidhub7 <louguikhalid7@gmail.com>
2024-11-10 09:25:43 -0800

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,12 +34,15 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        """ initialize """
         super(RedactingFormatter, self
               ).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord
                ) -> str:
+        """
+format the log record, redacting sensitive fields. """
         record.msg = filter_datum(
             self.fields, RedactingFormatter.REDACTION,
             record.msg, RedactingFormatter.SEPARATOR)
