607403b378e3c515a81ec89856a790c1255173fd
khalidhub7 <louguikhalid7@gmail.com>
2024-11-11 16:22:30 -0800

    error handler: unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route(
+    '/unauthorized', methods=['GET'],
+    strict_slashes=False)
+def unauthorized_page() -> str:
+    """ return unauthorized_page """
+    abort(401)
