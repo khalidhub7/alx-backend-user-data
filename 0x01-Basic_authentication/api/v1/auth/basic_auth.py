a0cb2d1390775668146c9697f1f49db4296d309c
khalidhub7 <louguikhalid7@gmail.com>
2024-11-12 16:27:45 -0800

    allow password with :

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,7 +50,7 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(
-            decoded_base64_authorization_header.split(':'))
+            decoded_base64_authorization_header.split(':', 1))
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str
