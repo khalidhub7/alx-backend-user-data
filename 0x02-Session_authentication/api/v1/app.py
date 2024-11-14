815ecce85c1c1edb96569a3636cde99bfdb026b4
khalidhub7 <louguikhalid7@gmail.com>
2024-11-14 05:02:55 -0800

    Sessions in database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,7 +28,7 @@ if auth_type == 'session_exp_auth':
     auth = SessionExpAuth()
 if auth_type == 'session_db_auth':
     from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
