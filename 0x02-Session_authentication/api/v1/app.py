e8f4112fbee7578ba5a4c3bcfac1acaec13e1f96
khalidhub7 <louguikhalid7@gmail.com>
2024-11-14 00:27:37 -0800

    (add a update)  before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,9 +52,10 @@ before each request '''
         if auth.require_auth(
             request.path,
             ['/api/v1/status/', '/api/v1/unauthorized/',
-             '/api/v1/forbidden/']):
-            if auth.authorization_header(
-                    request) is None:
+             '/api/v1/forbidden/', '/api/v1/auth_session/login/']):
+            if auth.authorization_header(request) \
+                is None and auth.session_cookie(request) \
+                    is None:
                 abort(401)
             request.current_user = auth.current_user(request)
             if auth.current_user(request) is None:
