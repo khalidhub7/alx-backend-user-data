8065692e4ce034427e7dec42280d95b814764516
khalidhub7 <louguikhalid7@gmail.com>
2024-11-19 16:44:52 -0800

    User  model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ (User)> SQLAlchemy model """
 from sqlalchemy import String, Integer, Column
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 """ from sqlalchemy import create_engine """
 Base = declarative_base()
 
@@ -14,3 +14,9 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+
+""" db_url = "mysql+pymysql://root:0000\
+@localhost/user_auth_test"
+engine = create_engine(db_url, echo=True)
+Base.metadata.create_all(engine) """
