dd9ca42a6d09f4dc673db702a1d6dd926bafb5d7
khalidhub7 <louguikhalid7@gmail.com>
2025-01-25 10:07:32 -0800

    user_id_for_session_id func

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,9 +14,19 @@ class SessionAuth(Auth):
 
     def create_session(
             self, user_id: str = None) -> str:
-        """ create and store a session ID for a user """
+        """ store user_id with session_id \
+            in user_id_by_session_id """
         if isinstance(user_id, str):
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+        return None
+
+    def user_id_for_session_id(
+            self, session_id: str = None) -> str:
+        """ return User id based on a session id """
+        if isinstance(session_id, str):
+            for i in reversed(self.user_id_by_session_id):
+                if i == session_id:
+                    return self.user_id_by_session_id.get(session_id)
         return None
