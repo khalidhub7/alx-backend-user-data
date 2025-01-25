644534e22b4b2837bf4b187ebce1677fe2badc89
khalidhub7 <louguikhalid7@gmail.com>
2025-01-25 10:15:20 -0800

    user_id_for_session_id func

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,7 +26,5 @@ class SessionAuth(Auth):
             self, session_id: str = None) -> str:
         """ return User id based on a session id """
         if isinstance(session_id, str):
-            for i in self.user_id_by_session_id:
-                if i == session_id:
-                    return self.user_id_by_session_id.get(session_id)
+            return self.user_id_by_session_id.get(session_id)
         return None
