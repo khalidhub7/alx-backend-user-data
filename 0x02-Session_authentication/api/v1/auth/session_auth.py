a0a22d94d22c6af2195e4232e15764252367494f
khalidhub7 <louguikhalid7@gmail.com>
2025-01-26 11:30:42 -0800

    current_user func in SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ session authentication """
 from uuid import uuid4
+from models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -28,3 +29,12 @@ class SessionAuth(Auth):
         if isinstance(session_id, str):
             return self.user_id_by_session_id.get(session_id)
         return None
+
+    def current_user(self, request=None):
+        """ returns user based on a cookie value """
+        try:
+            session_id = self.session_cookie(request)
+            user_id = self.user_id_for_session_id(session_id)
+            return User.get(user_id)
+        except Exception:
+            return None
