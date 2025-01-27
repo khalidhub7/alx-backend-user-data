389438757e74988c326be2ca8669bce2ceb46552
khalidhub7 <louguikhalid7@gmail.com>
2025-01-26 16:01:07 -0800

    destroy_session logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -46,7 +46,7 @@ def login():
     return res
 
 
-@app_views.route('/api/v1/auth_session/logout', methods=['DELETE'],
+@app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
 def logout():
     """ logout page """
