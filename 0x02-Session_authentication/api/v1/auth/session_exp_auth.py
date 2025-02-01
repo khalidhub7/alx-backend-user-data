544397bdd72c5f076e1808c2c85d0171cd18f226
khalidhub7 <louguikhalid7@gmail.com>
2025-02-01 07:15:28 -0800

    handle session expiration ##

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -13,16 +13,18 @@ class SessionExpAuth(SessionAuth):
         super()
         self.session_duration = int(getenv('SESSION_DURATION', '0'))
 
-    def create_session(self, user_id=None):
-        """ create session with expiration tracking """
+    def create_session(self, user_id: str = None) -> str:
+        """Create session method
+        """
         session_id = super().create_session(user_id)
-        if session_id:
-            self.user_id_by_session_id[session_id] = {
-                'user_id': user_id,
-                'created_at': datetime.now()
-            }
-            return session_id
-        return None
+        if session_id is None:
+            return None
+        session_dictionary = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
+        self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """ return 'session dict' based on a session id """
