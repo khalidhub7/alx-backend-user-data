c4173ab7b2255b75ed9d0d083b036db3714c301b
khalidhub7 <louguikhalid7@gmail.com>
2025-02-01 08:29:53 -0800

    handle session expiration ✅ done

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -10,8 +10,8 @@ class SessionExpAuth(SessionAuth):
 
     def __init__(self):
         """ initialize sess duration """
-        super()
-        self.session_duration = int(getenv('SESSION_DURATION', '0'))
+        self.session_duration = int(
+            getenv('SESSION_DURATION', '0'))
 
     def create_session(self, user_id=None):
         """ create session with expiration tracking """
