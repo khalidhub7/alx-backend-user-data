bd1770843c6acfa33d9921f9dc4e13360fe12ce3
khalidhub7 <louguikhalid7@gmail.com>
2025-02-08 22:19:47 +0100

    fix session in db

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python3
-""" session_db_auth module
-"""
-from models.user_session import UserSession
+""" session database auth module """
 from api.v1.auth.session_exp_auth import SessionExpAuth
-
-
-from os import getenv
+from models.user_session import UserSession
 
 
 class SessionDBAuth(SessionExpAuth):
-    """ session db auth class
-    """
+    """ database: file """
+
     def create_session(self, user_id=None):
-        """Create session method
-        """
+        """ create session but now stored in db not memory """
         session_id = super().create_session(user_id)
-        if session_id is None:
-            return None
-        user_session = UserSession(user_id=user_id, session_id=session_id)
-        user_session.save()
-        return session_id
+        if session_id and user_id:
+            user_session = UserSession(
+                user_id=user_id, session_id=session_id)
+            user_session.save()
+            return session_id
+        return None
 
     def user_id_for_session_id(self, session_id=None):
-        """User id for session id method
-        """
-        if session_id is None:
-            return None
-        if super().user_id_for_session_id(session_id) is None:
-            return None
-        user_session = UserSession.search({'session_id': session_id})
-        if user_session:
-            return user_session[0].user_id
+        """ user_id for given session_id """
+        if session_id:
+            # check is the session not yet expired
+            check_session = super().user_id_for_session_id(session_id)
+            if check_session:
+                user_session = UserSession.search(
+                    {'session_id': session_id})
+                if len(user_session) != 0:
+                    return user_session[0].user_id
         return None
 
     def destroy_session(self, request=None):
-        """Destroy session method
-        """
-        if not super().destroy_session(request):
-            return False
-        session_id = self.session_cookie(request)
-        if session_id:
+        """ delete user session (in database) / logout """
+        try:
+            # it means 'not implemented'—neither true nor false
+            if not super().destroy_session(request):
+                return None
+            session_id = self.session_cookie(request)
             user_session = UserSession.search({'session_id': session_id})
-            if user_session is None or user_session == []:
-                return False
-            user_session[0].remove()
-            return True
+            if len(user_session) != 0:
+                del user_session[0]
+                return True
+            raise Exception('session not found')
+        except Exception:
+            return False
