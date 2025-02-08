5e02a0224ec507f357d8c7e2b455ed780feefd8c
khalidhub7 <louguikhalid7@gmail.com>
2025-02-08 22:18:41 +0100

    fix session in db

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
-""" session database auth module """
-from api.v1.auth.session_exp_auth import SessionExpAuth
+""" session_db_auth module
+"""
 from models.user_session import UserSession
+from api.v1.auth.session_exp_auth import SessionExpAuth
+
+
+from os import getenv
 
 
 class SessionDBAuth(SessionExpAuth):
-    """ database: file """
-
+    """ session db auth class
+    """
     def create_session(self, user_id=None):
-        """ create session but now stored in db not memory """
+        """Create session method
+        """
         session_id = super().create_session(user_id)
-        if session_id and user_id:
-            user_session = UserSession(
-                user_id=user_id, session_id=session_id)
-            user_session.save()
-            return session_id
-        return None
+        if session_id is None:
+            return None
+        user_session = UserSession(user_id=user_id, session_id=session_id)
+        user_session.save()
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """ user_id for given session_id """
-        if session_id:
-            # check is the session not yet expired
-            check_session = super().user_id_for_session_id(session_id)
-            if check_session:
-                user_session = UserSession.search(
-                    {'session_id': session_id})
-                if len(user_session) != 0:
-                    return user_session[0].user_id
+        """User id for session id method
+        """
+        if session_id is None:
+            return None
+        if super().user_id_for_session_id(session_id) is None:
+            return None
+        user_session = UserSession.search({'session_id': session_id})
+        if user_session:
+            return user_session[0].user_id
         return None
 
     def destroy_session(self, request=None):
