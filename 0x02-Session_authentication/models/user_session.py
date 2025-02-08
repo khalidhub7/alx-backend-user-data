6a49cff65fb53adc36356281439125cbaadce0d0
khalidhub7 <louguikhalid7@gmail.com>
2025-02-08 22:30:14 +0100

    fix session in db

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""
-user session module
-"""
+""" usersessiondb module  """
+
 from models.base import Base
 
 
 class UserSession(Base):
-    """UserSession class
-    """
+    """ usersession class """
+
     def __init__(self, *args: list, **kwargs: dict):
-        """Constructor
-        """
+        """ constructor """
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get("user_id")
-        self.session_id = kwargs.get("session_id")
+        self.user_id = str(kwargs.get('user_id'))
+        self.session_id = str(kwargs.get('session_id'))
