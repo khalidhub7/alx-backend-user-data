86e30ee23c5d5a484df10cb8d84c32a9c60375d1
khalidhub7 <louguikhalid7@gmail.com>
2025-02-14 04:41:05 +0100

    clear

--- 0x02-Session_authentication/auth_header.js
+++
@@ -1,8 +0,0 @@
-const credentials = btoa("username:password");  // Base64 encode
-localStorage.setItem('auth', credentials);  // Store securely
-
-fetch("/api/v1/protected-route", {
-    headers: {
-        "Authorization": `Basic ${localStorage.getItem('auth')}`
-    }
-});
