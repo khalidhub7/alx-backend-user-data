ba5bb958c79c1a1aedc772c0d4832f04e2cc7aa1
khalidhub7 <louguikhalid7@gmail.com>
2025-02-14 03:48:42 +0100

    fix

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -15,20 +15,19 @@ class SessionDBAuth(SessionExpAuth):
                 user_session = UserSession(
                     user_id=user_id, session_id=session_id)
                 user_session.save()
-                print(user_session)
                 return session_id
         return None
 
     def user_id_for_session_id(self, session_id=None):
-        """ user_id for given session_id """
-        if session_id:
-            found_sessions = UserSession.search({'session_id': session_id})
-            if len(found_sessions) != 0:
-                # check if session not_yet expired
-                '''if super().user_id_for_session_id(session_id):'''
-                check = super().user_id_for_session_id(session_id)
-                if check:
-                    return found_sessions[0].user_id
+        """User id for session id method
+        """
+        if session_id is None:
+            return None
+        if super().user_id_for_session_id(session_id) is None:
+            return None
+        user_session = UserSession.search({'session_id': session_id})
+        if user_session:
+            return user_session[0].user_id
         return None
 
     def destroy_session(self, request=None):
--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -26,25 +26,21 @@ class SessionExpAuth(SessionAuth):
             return session_id
         return None
 
-    def user_id_for_session_id(self, session_id=None):
+    def user_id_for_session_id(
+            self,
+            session_id: str = None) -> str:
+        """User id for session id method
         """
-return 'user_id' based on session_id, session_dict """
-        try:
-            if session_id:
-                session_dict = self.user_id_by_session_id.get(session_id)
-                user_id = session_dict.get('user_id')
-                if session_dict and user_id:
-                    # no expiration permanent session
-                    if self.session_duration <= 0:
-                        return user_id
-
-                    # with expiration
-                    created_at = session_dict.get('created_at')
-                    expiration_date = created_at + timedelta(
-                        seconds=self.session_duration)  # enta tssali session
-                    if datetime.now() > expiration_date:
-                        raise Exception("session expired")
-                    return user_id
-            raise Exception('no session_id')
-        except Exception:
+        if session_id is None or type(session_id) is not str:
             return None
+        session_dictionary = self.user_id_by_session_id.get(session_id)
+        if session_dictionary is None:
+            return None
+        if self.session_duration <= 0:
+            return session_dictionary.get("user_id")
+        if "created_at" not in session_dictionary:
+            return None
+        if (datetime.now() - session_dictionary.get("created_at")
+                > timedelta(seconds=self.session_duration)):
+            return None
+        return session_dictionary.get("user_id")
