4f91c6871f502479ec0d739564ad5940af39e8c9
khalidhub7 <louguikhalid7@gmail.com>
2025-02-15 11:41:56 +0100

    done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,45 +1,51 @@
 #!/usr/bin/env python3
-""" login / logout """
-
-from flask import request, jsonify
-from api.v1.views import app_views
+""" session authentication module """
+from api.v1.auth.auth import Auth
 
 
-@app_views.route('/auth_session/login',
-                 strict_slashes=False, methods=['POST'])
-def login():
-    """ login route """
-    email = request.form.get('email')
-    password = request.form.get('password')
+class SessionAuth(Auth):
+    """ Session Auth class """
+    user_id_by_session_id = {}
 
-    if not email or len(email) == 0:
-        return jsonify({"error": "email missing"}), 400
-    if not password or len(password) == 0:
-        return jsonify({"error": "password missing"}), 400
-    from models.user import User
-    found_users = User.search({'email': email})
-    if len(found_users) != 0:
-        for user in found_users:
-            if user.is_valid_password(password):
-                # create a session
-                from api.v1.app import auth
-                from os import getenv
-                from flask import make_response
-                session_id = auth.create_session(user.id)
-                cookie_key = getenv('SESSION_NAME')
-                response = make_response(jsonify(user.to_json()), 200)
-                response.set_cookie(cookie_key, session_id)
-                return response
-            return jsonify({"error": "wrong password"}), 401
-    return jsonify({"error": "no user found for this email"}), 404
+    def create_session(self, user_id: str = None) -> str:
+        """ create session for a given user_id """
+        if user_id and isinstance(user_id, str):
+            from uuid import uuid4
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
 
+    def user_id_for_session_id(
+            self, session_id: str = None) -> str:
+        """ return a user_id for given session_id """
+        if session_id and isinstance(session_id, str):
+            return self.user_id_by_session_id.get(session_id)
+        return None
 
-@app_views.route('/auth_session/logout',
-                 strict_slashes=False, methods=['DELETE'])
-def logout():
-    """ logout route """
-    from api.v1.app import auth
-    from flask import abort
-    if auth.destroy_session(request):
-        return jsonify({}), 200
-    abort(404)
+    def current_user(self, request=None):
+        """ return user obj based on session_id """
+        if request:
+            session_id = self.session_cookie(request)
+            if session_id:
+                # here it use appropriate (user_id_for_session_id) func
+                # (user_id_for_session_id) that control expiration
+                # so (session_auth) or (session_exp_auth)'s
+                # (user_id_for_session_id)
+                user_id = self.user_id_for_session_id(session_id)
+                if user_id:
+                    from models.user import User
+                    user = User.get(user_id)
+                    if user:
+                        return user
+        return None
+
+    def destroy_session(self, request=None):
+        """ deletes the user session / logout """
+        if request:
+            session_id = self.session_cookie(request)
+            if session_id and session_id \
+                    in self.user_id_by_session_id:
+                del self.user_id_by_session_id[session_id]
+                return True
+        return False
