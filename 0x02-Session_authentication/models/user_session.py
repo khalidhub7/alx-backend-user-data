0b339c39cf15abb4b8cfb778b9ca564181373050
khalidhub7 <louguikhalid7@gmail.com>
2025-02-15 09:56:43 +0100

    store sessions in database

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-""" store Session IDs in db (file) """
+"""
+user session module
+"""
 from models.base import Base
 
 
 class UserSession(Base):
-    """ user session class """
-
+    """UserSession class
+    """
     def __init__(self, *args: list, **kwargs: dict):
-        super().__init__(*args, kwargs)
-        self.user_id = kwargs.get('user_id', '')
-        self.session_id = kwargs.get('session_id', '')
+        """Constructor
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
