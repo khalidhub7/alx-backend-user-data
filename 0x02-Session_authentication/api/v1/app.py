da6f9c7d8a99e0b7f37b6bc204ccaf823b720061
khalidhub7 <louguikhalid7@gmail.com>
2025-02-15 04:17:51 +0100

    setup SessionAuth class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,6 +21,9 @@ if auth_type:
     if auth_type == 'basic_auth':
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
+    if auth_type == 'session_auth':
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
 
 
 @app.errorhandler(404)
