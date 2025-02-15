e4523aa8ac6132c1ec4d7c081511aff6899a6723
khalidhub7 <louguikhalid7@gmail.com>
2025-02-15 05:49:34 +0100

    handle login

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" login / logout """
+
+from flask import request, jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/auth_session/login',
+                 strict_slashes=False, methods=['POST'])
+def login():
+    """ login route """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+
+    if not email or len(email) == 0:
+        return jsonify({"error": "email missing"}), 400
+    if not password or len(password) == 0:
+        return jsonify({"error": "password missing"}), 400
+    from models.user import User
+    found_users = User.search({'email': email})
+    if len(found_users) != 0:
+        for user in found_users:
+            if user.is_valid_password(password):
+                # create a session
+                from api.v1.app import auth
+                from os import getenv
+                from flask import make_response
+                session_id = auth.create_session(user.id)
+                cookie_key = getenv('SESSION_NAME')
+                response = make_response(jsonify(user.to_json()), 200)
+                response.set_cookie(cookie_key, session_id)
+                return response
+            return jsonify({"error": "wrong password"}), 401
+    return jsonify({"error": "no user found for this email"}), 404
