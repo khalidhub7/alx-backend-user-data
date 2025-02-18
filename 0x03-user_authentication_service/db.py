e9d5a040238f9a63abdbfecca2a3c70de368d457
khalidhub7 <louguikhalid7@gmail.com>
2025-02-18 20:53:07 +0100

    create new user (row) in db method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -12,7 +12,11 @@ class DB:
 
     def __init__(self) -> None:
         """ initialize a new DB instance """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine(
+            "sqlite:///a.db", echo=False)
+        # self._engine = create_engine(
+        #    "mysql://khalid77:0000@localhost/user_auth", echo=False)
+
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -25,10 +29,10 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
-        """ add_user to the users table """
-        user = User(email=email,
-                    hashed_password=hashed_password)
-        self._session.add(user)
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+        return new_user
