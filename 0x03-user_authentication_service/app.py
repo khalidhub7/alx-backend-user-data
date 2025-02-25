5ee9556b5275f7298248013c5a89dc1765d8637a
khalidhub7 <louguikhalid7@gmail.com>
2025-02-25 16:45:33 +0100

    register route ##?

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 """ main app """
+from wrappers import wrap_register
 from flask import Flask, jsonify
+from auth import Auth
+AUTH = Auth()
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False, methods=['GET'])
+@app.route('/', strict_slashes=False,
+           methods=['GET'])
 def index():
     """ basic route """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', strict_slashes=False,
+           methods=['POST'])
+@wrap_register
+def register(email, password):
+    """ register route 'page' """
+    return AUTH.register_user(email, password)
 
 
 if __name__ == '__main__':
