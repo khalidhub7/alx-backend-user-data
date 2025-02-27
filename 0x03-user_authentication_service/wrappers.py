ec63b5f707a20060d2b2b13482284defd559feda
khalidhub7 <louguikhalid7@gmail.com>
2025-02-27 20:18:19 +0100

    log out route

--- 0x03-user_authentication_service/wrappers.py
+++ 0x03-user_authentication_service/wrappers.py
@@ -2,18 +2,18 @@
 """ wrapper functions to modify
 route behavior """
 from flask import request, jsonify, abort
+from functools import wraps
 from auth import Auth
 AUTH = Auth()
-
-# request credentials
-email = request.form.get('email')
-password = request.form.get('password')
 
 
 # register_route
 def wrap_register(func):
     """ register_route behavior """
+    @wraps(func)
     def wrapped_register():
+        email = request.form.get('email')
+        password = request.form.get('password')
         if not email or not password:
             return jsonify({
                 "message": "missing email or password"
@@ -33,8 +33,11 @@ def wrap_register(func):
 # login_route
 def wrap_login(func):
     """ login_route behavior """
+    @wraps(func)
     def wrapped_login():
         try:
+            email = request.form.get('email')
+            password = request.form.get('password')
             # validate credentials
             isvalid = AUTH.valid_login(email, password)
             if isvalid:
@@ -53,13 +56,14 @@ def wrap_login(func):
 
 def wrap_logout(func):
     """ logout_route behavior """
+    @wraps(func)
     def wrapped_logout():
         try:
             sess_cookie = request.cookies.get('session_id')
             user = AUTH.get_user_from_session_id(sess_cookie)
             AUTH.destroy_session(user.id)
-            from flask import redirect
-            redirect('app.sessions')
+            from flask import redirect, url_for
+            return redirect(url_for('sessions'))
         except Exception:
             abort(403)
     return wrapped_logout
