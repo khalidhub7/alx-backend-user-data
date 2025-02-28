d8b4709b237cdbd868cb9a300c238e2f1e69f5c7
khalidhub7 <louguikhalid7@gmail.com>
2025-02-28 02:47:28 +0100

    update password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,7 +90,7 @@ with the authentication database. """
             user = self._db.find_user_by(reset_token=reset_token)
             if user:
                 hashpwd = _hash_password(password)
-                self._db.update_user(hashed_password=hashpwd,
+                self._db.update_user(user.id, hashed_password=hashpwd,
                                      reset_token=None)
                 return None
         except Exception:
